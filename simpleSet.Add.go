8b3e5cca770ac68790974fa59d383d1943785068
sam caldwell <mail@samcaldwell.net>
2023-06-05 19:31:57 -0500

    fix simpleSet to not push a nil item

--- simpleSet.Add.go
+++ simpleSet.Add.go
@@ -7,6 +7,9 @@ import (
 
 // Add - add item to set.
 func (set *Set) Add(item interface{}) error {
+	if item == nil {
+		return nil
+	}
 	if set.typ == nil {
 		// Initialize the set's type with the type of the first item
 		set.typ = reflect.TypeOf(item)
